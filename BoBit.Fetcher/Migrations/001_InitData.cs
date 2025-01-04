3f6f4a8cf9d94715515112725a5bbe74a7c570c5
Bohdan Kovalevych <bohdan.kovalevych@outlook.com>
2025-01-04 13:52:33 +0200

    init migration

--- BoBit.Fetcher/Migrations/001_InitData.cs
+++ BoBit.Fetcher/Migrations/001_InitData.cs
@@ -1,18 +1,34 @@
 ﻿using FluentMigrator;
+using FluentMigrator.SqlServer;
 
 namespace BoBit.Fetcher.Migrations
 {
-    //[Migration(20250103)]
-    public class _001_InitData //: Migration
+    [Migration(20250103)]
+    public class InitData : Migration
     {
-        //public override void Up()
-        //{
-        //
-        //}
+        public override void Up()
+        {
+            Create.Table("BitcoinPrices")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Created").AsDateTimeOffset().NotNullable()
+                .WithColumn("Timestamp").AsDateTimeOffset().NotNullable()
+                .WithColumn("CryptoCurrency").AsFixedLengthString(32).NotNullable()
+                .WithColumn("FiatCurrency").AsFixedLengthString(32).NotNullable()
+                .WithColumn("Price").AsDecimal(18, 8).NotNullable();
 
-        //public override void Down() 
-        //{
-        //    
-        //}
+            Create.Index("IX_BitcoinPrices_Timestamp")
+                .OnTable("BitcoinPrices")
+                .OnColumn("Timestamp").Descending()
+                .Include("Price")
+                .Include("FiatCurrency")
+                .Include("CryptoCurrency");
+        
+        }
+
+        public override void Down()
+        {
+            Delete.Index("IX_BitcoinPrices_Timestamp");
+            Delete.Table("BitcoinPrices");
+        }
     }
 }
